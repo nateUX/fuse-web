0396b31e846597422d5eb1a518a360f70c62a792
nateUX <nateUX@users.noreply.github.com>
2018-10-21 08:59:34 -0500

    Updating typgraphy page

--- src/app/typography/typography.component.ts
+++ src/app/typography/typography.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-typography',
   templateUrl: './typography.component.html',
   styleUrls: ['./typography.component.css']
 })
 export class TypographyComponent implements OnInit {
-
+  step = 1;
   constructor() { }
 
   ngOnInit() {
